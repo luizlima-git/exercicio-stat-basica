54bf7c4db27d6da2c213d8d3eed86bc2494b4817
Luiz Lima <luizlima.git@gmail.com>
2026-03-21 20:50:18 -0300

    Alterando a definição para 50 elementos:

--- analise.R
+++ analise.R
@@ -1,13 +1,15 @@
 # Definindo um vetor com 100 números aleatórios entre 1 e 100
 
+# Alterando a definição para 50 elementos:
+
 # sample(x, size, replace = FALSE)
-# conjunto de elementos a sortear (ex.: 1:100)
+# conjunto de elementos a sortear (ex.: 1:50)
 # size → quantos elementos você quer sortear
 # replace (se TRUE, permite repetir elementos; 
 #          se FALSE, cada elemento só pode aparecer uma vez)
 
-# Gerar 100 números inteiros aleatórios entre 1 e 100
-vetor_aleatorio <- sample(1:100, size = 100, replace = TRUE)
+# Gerar 50 números inteiros aleatórios entre 1 e 50
+vetor_aleatorio <- sample(1:100, size = 50, replace = TRUE)
 
 # Mostrar os primeiros valores
 head(vetor_aleatorio)
